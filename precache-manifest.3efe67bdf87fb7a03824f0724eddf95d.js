c50d62a5e3824ecc06ee0a39b8a3e6cbfd273781
boh001 <boh001g@gmail.com>
2020-04-15 19:12:12 +0900

    Updates

---
+++ precache-manifest.3efe67bdf87fb7a03824f0724eddf95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a47f79f3c0d2e490b67767a0fdb13d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97003ec2d36108ed902b",
+    "url": "/portfolio/static/js/2.8506cab6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.8506cab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6290bd8d9a1fd1683bcf",
+    "url": "/portfolio/static/js/main.65f44dcd.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "62063fb81903a643584f579ece46c84b",
+    "url": "/portfolio/static/media/kimbook-react.62063fb8.PNG"
+  },
+  {
+    "revision": "9e708993598a95063a39d9f1e554ec60",
+    "url": "/portfolio/static/media/weathertodo.9e708993.png"
+  }
+]);
